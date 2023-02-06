4c8321729120fa5b3251d17bfb385e297e3a7285
David Dias <thedaviddias@gmail.com>
2023-02-05 20:02:08 -0500

    feat: add lint-staged

---
+++ config/lint-staged/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup'
+import type { Format, Options } from 'tsup'
+
+import { config as _config } from '../../tsup.config'
+
+const entry = ['src/*.ts']
+const format: Format[] = ['esm']
+
+const config: Options = {
+  ..._config,
+  dts: false,
+  format,
+  entry,
+}
+
+export default defineConfig({
+  ...config,
+})
